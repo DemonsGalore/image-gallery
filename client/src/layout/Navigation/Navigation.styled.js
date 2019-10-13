2fa12e0b249064dc4a2fa027aef76de341649203
Andreas Stopat <dev@demonsgalore.de>
2019-10-13 21:05:20 +0200

    style fix

--- client/src/layout/Navigation/Navigation.styled.js
+++ client/src/layout/Navigation/Navigation.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledNavigation = styled.nav`
   display: flex;
-  justify-content: end;
+  justify-content: flex-end;
 
   ul {
     list-style: none;
